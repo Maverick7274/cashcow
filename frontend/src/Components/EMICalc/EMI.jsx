d2b2b11aa20082cf319bd562317e326639460807
Neelanjan Mukherji <neelanjanmukherji@gmail.com>
2024-01-27 14:37:57 +0530

    EMI Calc Styling partial

--- frontend/src/Components/EMICalc/EMI.jsx
+++ frontend/src/Components/EMICalc/EMI.jsx
@@ -1,168 +1,150 @@
 import React, { useEffect, useState } from "react";
-import LoggedInNavbar from '../Navbar/LoggedInNavbar';
+import LoggedInNavbar from "../Navbar/LoggedInNavbar";
 import { tenureData } from "./utils/constants";
 import { numberWithCommas } from "./utils/config";
 import TextInput from "./components/text-input";
 import SliderInput from "./components/slider-input";
 
-
 export function EMI() {
-	const [cost, setCost] = useState(0);
-	const [interest, setInterest] = useState(10);
-	const [fee, setFee] = useState(1);
-	const [downPayment, setDownPayment] = useState(0);
-	const [tenure, setTenure] = useState(12);
-	const [emi, setEmi] = useState(0);
+  const [cost, setCost] = useState(0);
+  const [interest, setInterest] = useState(10);
+  const [fee, setFee] = useState(1);
+  const [downPayment, setDownPayment] = useState(0);
+  const [tenure, setTenure] = useState(12);
+  const [emi, setEmi] = useState(0);
 
-	const submitHandler = (e) => {
-		e.preventDefault();
+  const submitHandler = (e) => {
+    e.preventDefault();
 
-		console.log(cost)
-	}
+    console.log(cost);
+  };
 
-	const calculateEMI = (downpayment) => {
-		// EMI amount = [P x R x (1+R)^N]/[(1+R)^N-1]
-		if (!cost) return;
+  const calculateEMI = (downpayment) => {
+    // EMI amount = [P x R x (1+R)^N]/[(1+R)^N-1]
+    if (!cost) return;
 
-		const loanAmt = cost - downpayment;
-		const rateOfInterest = interest / 100;
-		const numOfYears = tenure / 12;
+    const loanAmt = cost - downpayment;
+    const rateOfInterest = interest / 100;
+    const numOfYears = tenure / 12;
 
-		const EMI =
-			(loanAmt * rateOfInterest * (1 + rateOfInterest) ** numOfYears) /
-			((1 + rateOfInterest) ** numOfYears - 1);
+    const EMI =
+      (loanAmt * rateOfInterest * (1 + rateOfInterest) ** numOfYears) /
+      ((1 + rateOfInterest) ** numOfYears - 1);
 
-		return Number(EMI / 12).toFixed(0);
-	};
+    return Number(EMI / 12).toFixed(0);
+  };
 
-	const calculateDP = (emi) => {
-		if (!cost) return;
+  const calculateDP = (emi) => {
+    if (!cost) return;
 
-		const downPaymentPercent = 100 - (emi / calculateEMI(0)) * 100;
-		return Number((downPaymentPercent / 100) * cost).toFixed(0);
-	};
+    const downPaymentPercent = 100 - (emi / calculateEMI(0)) * 100;
+    return Number((downPaymentPercent / 100) * cost).toFixed(0);
+  };
 
-	useEffect(() => {
-		if (!(cost > 0)) {
-			setDownPayment(0);
-			setEmi(0);
-		}
+  useEffect(() => {
+    if (!(cost > 0)) {
+      setDownPayment(0);
+      setEmi(0);
+    }
 
-		const emi = calculateEMI(downPayment);
-		setEmi(emi);
-	}, [tenure, cost]);
+    const emi = calculateEMI(downPayment);
+    setEmi(emi);
+  }, [tenure, cost]);
 
-	const updateEMI = (e) => {
-		if (!cost) return;
+  const updateEMI = (e) => {
+    if (!cost) return;
 
-		const dp = Number(e.target.value);
-		setDownPayment(dp.toFixed(0));
+    const dp = Number(e.target.value);
+    setDownPayment(dp.toFixed(0));
 
-		const emi = calculateEMI(dp);
-		setEmi(emi);
-	};
-	const updateDownPayment = (e) => {
-		if (!cost) return;
+    const emi = calculateEMI(dp);
+    setEmi(emi);
+  };
+  const updateDownPayment = (e) => {
+    if (!cost) return;
 
-		const emi = Number(e.target.value);
-		setEmi(emi.toFixed(0));
+    const emi = Number(e.target.value);
+    setEmi(emi.toFixed(0));
 
-		const dp = calculateDP(emi);
-		setDownPayment(dp);
-	};
+    const dp = calculateDP(emi);
+    setDownPayment(dp);
+  };
 
-	const totalDownPayment = () => {
-		return numberWithCommas(
-			(Number(downPayment) + (cost - downPayment) * (fee / 100)).toFixed(0)
-		);
-	};
+  const totalDownPayment = () => {
+    return numberWithCommas(
+      (Number(downPayment) + (cost - downPayment) * (fee / 100)).toFixed(0)
+    );
+  };
 
-	const totalEMI = () => {
-		return numberWithCommas((emi * tenure).toFixed(0));
-	};
+  const totalEMI = () => {
+    return numberWithCommas((emi * tenure).toFixed(0));
+  };
 
-	// return (
-	// 	<div>
-	// 		<LoggedInNavbar />
-	// 		<div className="flex justify-center items-center min-h-screen sm:flex-row flex-col">
-	// 			<form onSubmit={submitHandler} class="flex flex-col rounded-lg shadow-sm">
-	// 				<span className="px-4 inline-flex items-center min-w-fit rounded-s-md border border-e-0 border-gray-200 bg-gray-50 text-sm text-gray-500 dark:bg-gray-700 dark:border-gray-700 dark:text-gray-400">Total Cost of Asset</span>
-	// 				<input value={cost} onChange={(e) => {setCost(e.target.value)}} type="text" className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm rounded-e-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" />
-	// 				<span className="px-4 inline-flex items-center min-w-fit rounded-s-md border border-e-0 border-gray-200 bg-gray-50 text-sm text-gray-500 dark:bg-gray-700 dark:border-gray-700 dark:text-gray-400">Interest Rate in %</span>
-	// 				<input type="text" className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm rounded-e-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" />
-	// 				<span className="px-4 inline-flex items-center min-w-fit rounded-s-md border border-e-0 border-gray-200 bg-gray-50 text-sm text-gray-500 dark:bg-gray-700 dark:border-gray-700 dark:text-gray-400">Down Payment</span>
-	// 				<input type="text" className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm rounded-e-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" />
-	// 				<span className="px-4 inline-flex items-center min-w-fit rounded-s-md border border-e-0 border-gray-200 bg-gray-50 text-sm text-gray-500 dark:bg-gray-700 dark:border-gray-700 dark:text-gray-400">Total Loan Amount</span>
-	// 				<input type="text" className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm rounded-e-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" />
-	// 				<span className="px-4 inline-flex items-center min-w-fit rounded-s-md border border-e-0 border-gray-200 bg-gray-50 text-sm text-gray-500 dark:bg-gray-700 dark:border-gray-700 dark:text-gray-400">Tenure</span>
-	// 				<input type="text" className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm rounded-e-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" />
-	// 				<button>Submit</button>
-	// 			</form>
-	// 		</div>
-	// 	</div>
-	// )
+  return (
+    <div className="w-full">
+      <LoggedInNavbar />
+      <div className="flex flex-col justify-center items-center w-full h-screen">
+        <span className="text-4xl ">EMI Calculator</span>
 
-	return (
-		<div className="flex flex-col justify-center items-center">
-			<LoggedInNavbar />
-			<div className="flex flex-col">
-				<span className="title" style={{ fontSize: 30, marginTop: 10 }}>
-					EMI Calculator
-				</span>
+        <div className="flex justify-center items-center flex-col gap-5 my-5">
+          <TextInput
+            title={"Total Cost of Asset"}
+            state={cost}
+            setState={setCost}
+          />
 
-				<TextInput
-					title={"Total Cost of Asset"}
-					state={cost}
-					setState={setCost}
-				/>
+          <TextInput
+            title={"Interest Rate (in %)"}
+            state={interest}
+            setState={setInterest}
+          />
 
-				<TextInput
-					title={"Interest Rate (in %)"}
-					state={interest}
-					setState={setInterest}
-				/>
+          <TextInput
+            title={"Processing Fee (in %)"}
+            state={fee}
+            setState={setFee}
+          />
+        </div>
 
-				<TextInput
-					title={"Processing Fee (in %)"}
-					state={fee}
-					setState={setFee}
-				/>
+        <div>
+          <SliderInput
+            title="Down Payment"
+            underlineTitle={`Total Down Payment - ${totalDownPayment()}`}
+            onChange={updateEMI}
+            state={downPayment}
+            min={0}
+            max={cost}
+            labelMin={"0%"}
+            labelMax={"100%"}
+          />
 
-				<SliderInput
-					title="Down Payment"
-					underlineTitle={`Total Down Payment - ${totalDownPayment()}`}
-					onChange={updateEMI}
-					state={downPayment}
-					min={0}
-					max={cost}
-					labelMin={"0%"}
-					labelMax={"100%"}
-				/>
+          <SliderInput
+            title="Loan per Month"
+            underlineTitle={`Total Loan Amount - ${totalEMI()}`}
+            onChange={updateDownPayment}
+            state={emi}
+            min={calculateEMI(cost)}
+            max={calculateEMI(0)}
+          />
+        </div>
 
-				<SliderInput
-					title="Loan per Month"
-					underlineTitle={`Total Loan Amount - ${totalEMI()}`}
-					onChange={updateDownPayment}
-					state={emi}
-					min={calculateEMI(cost)}
-					max={calculateEMI(0)}
-				/>
-
-				<span className="title">Tenure</span>
-				<div className="tenureContainer">
-					{tenureData.map((t) => {
-						return (
-							<button
-								className={`tenure ${t === tenure ? "selected" : ""}`}
-								onClick={() => setTenure(t)}>
-								{t}
-							</button>
-						);
-					})}
-				</div>
-			</div>
-		</div>
-)
+        <span className="">Tenure</span>
+        <div className="flex gap-7 items-center justify-center">
+          {tenureData.map((t) => {
+            return (
+              <button
+                key={t}
+                className={`py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-gray-200 text-gray-500 hover:border-blue-600 hover:text-blue-600 disabled:opacity-50 disabled:pointer-events-none dark:border-gray-700 dark:text-gray-400 dark:hover:text-blue-500 dark:hover:border-blue-600 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600`}
+                onClick={() => setTenure(t)}
+              >
+                {t}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default EMI
+export default EMI;
--- frontend/src/Components/EMICalc/components/text-input.jsx
+++ frontend/src/Components/EMICalc/components/text-input.jsx
@@ -1,17 +1,89 @@
-import React from "react";
-
 function TextInput({ title, state, setState }) {
-	return (
-		<React.Fragment>
-			<span className="title">{title}</span>
-			<input
-				type="number"
-				value={state}
-				onChange={(e) => setState(e.target.value)}
-				placeholder={title}
-			/>
-		</React.Fragment>
-	);
+  return (
+    <div className="">
+      <h1 className="">{title}</h1>
+      {/* <div className="">
+        <input
+			className="peer py-3 px-4 ps-11 block w-full bg-gray-100 border-transparent rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-700 dark:border-transparent dark:text-gray-400 dark:focus:ring-gray-600"
+			type="number"
+			value={state}
+			onChange={(e) => setState(e.target.value)}
+			placeholder={title}
+        />
+        <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none ps-4 peer-disabled:opacity-50 peer-disabled:pointer-events-none">
+          <svg
+            className="flex-shrink-0 w-4 h-4 text-gray-500"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+            <circle cx="12" cy="7" r="4" />
+          </svg>
+        </div>
+      </div> */}
+      <div
+        className="bg-white border border-gray-200 rounded-lg dark:bg-slate-700 dark:border-gray-700"
+        data-hs-input-number
+      >
+        <div className="w-full flex justify-between items-center gap-x-1">
+          <div className="grow py-2 px-3">
+            <input
+              className="w-full p-0 bg-transparent border-0 text-gray-800 focus:ring-0 dark:text-white"
+              type="number"
+              value={state}
+              onChange={(e) => setState(e.target.value)}
+              placeholder={title}
+              data-hs-input-number-input
+            />
+          </div>
+          <div className="flex items-center -gap-y-px divide-x divide-gray-200 border-s border-gray-200 dark:divide-gray-700 dark:border-gray-700">
+            <button
+              type="button"
+              className="w-10 h-10 inline-flex justify-center items-center gap-x-2 text-sm font-medium last:rounded-e-lg bg-white text-gray-800 hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+              data-hs-input-number-decrement
+            >
+              <svg
+                className="flex-shrink-0 w-3.5 h-3.5"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+              >
+                <path d="M5 12h14" />
+              </svg>
+            </button>
+            <button
+              type="button"
+              className="w-10 h-10 inline-flex justify-center items-center gap-x-2 text-sm font-medium last:rounded-e-lg bg-white text-gray-800 hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+              data-hs-input-number-increment
+            >
+              <svg
+                className="flex-shrink-0 w-3.5 h-3.5"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+              >
+                <path d="M5 12h14" />
+                <path d="M12 5v14" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TextInput;
